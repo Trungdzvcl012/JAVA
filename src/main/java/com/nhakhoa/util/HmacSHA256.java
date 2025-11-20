623a172a03857a19ab78da40e432250be4da18fd
Lê Quốc Trung <142233178+Trungdzvcl012@users.noreply.github.com>
2025-11-20 22:16:00 +0700

    Update HmacSHA256.java

--- src/main/java/com/nhakhoa/util/HmacSHA256.java
+++ src/main/java/com/nhakhoa/util/HmacSHA256.java
@@ -1,1 +1,26 @@
+package com.nhakhoa.util;
 
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class HmacSHA256 {
+
+    public static String hash(String key, String data) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+
+        byte[] hashBytes = sha256_HMAC.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(hashBytes);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        return sb.toString();
+    }
+}
